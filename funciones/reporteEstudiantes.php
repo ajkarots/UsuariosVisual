dce4bf94eae937812a753bd2a617b764dd824fb3
ajkarots <jonathan130397@outlook.es>
2025-07-01 15:15:00 -0500

    correcion

--- funciones/reporteEstudiantes.php
+++ funciones/reporteEstudiantes.php
@@ -8,11 +8,11 @@ $pdf = new FPDF('P', 'mm', 'A4');
 $pdf->AddPage();
 $pdf->SetTitle("ESTUDIANTES");
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(20,10, ('Cédula'),     1);
-$pdf->Cell(30,10, ('Nombre'),     1);
-$pdf->Cell(30,10, ('Apellido'),   1);
-$pdf->Cell(30,10, ('Teléfono'),   1);
-$pdf->Cell(60,10, ('Dirección'),  1);
+$pdf->Cell(20,10, iconv('UTF-8', 'ISO-8859-1//TRANSLIT','Cédula'),     1);
+$pdf->Cell(30,10, iconv('UTF-8', 'ISO-8859-1//TRANSLIT','Nombre'),     1);
+$pdf->Cell(30,10, iconv('UTF-8', 'ISO-8859-1//TRANSLIT','Apellido'),   1);
+$pdf->Cell(30,10, iconv('UTF-8', 'ISO-8859-1//TRANSLIT','Teléfono'),   1);
+$pdf->Cell(60,10, iconv('UTF-8', 'ISO-8859-1//TRANSLIT','Dirección'),  1);
 $pdf->Ln();
 while($fila=$stmt->fetch_object()){
     $CED_EST = $fila->CED_EST;
@@ -20,11 +20,11 @@ while($fila=$stmt->fetch_object()){
     $APE_EST = $fila->APE_EST;
     $TEL_EST = $fila->TEL_EST;
     $DIR_EST = $fila->DIR_EST;
-    $pdf->Cell(20,10,($CED_EST),1);
-    $pdf->Cell(30,10,($NOM_EST),1);
-    $pdf->Cell(30,10,($APE_EST),1);
-    $pdf->Cell(30,10,($TEL_EST),1);
-    $pdf->Cell(60,10,($DIR_EST),1);
+    $pdf->Cell(20,10,iconv('UTF-8', 'ISO-8859-1//TRANSLIT',$CED_EST),1);
+    $pdf->Cell(30,10,iconv('UTF-8', 'ISO-8859-1//TRANSLIT',$NOM_EST),1);
+    $pdf->Cell(30,10,iconv('UTF-8', 'ISO-8859-1//TRANSLIT',$APE_EST),1);
+    $pdf->Cell(30,10,iconv('UTF-8', 'ISO-8859-1//TRANSLIT',$TEL_EST),1);
+    $pdf->Cell(60,10,iconv('UTF-8', 'ISO-8859-1//TRANSLIT',$DIR_EST),1);
     $pdf->Ln();
 }
 $pdf->Output('D', 'reporte_estudiantes.pdf');  
